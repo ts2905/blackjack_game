dab6602bb15e73c912e758fd2e1cc6db4c4b300c
ts2905 <84340990+ts2905@users.noreply.github.com>
2021-06-27 12:53:15 -0400

    Completed assertions in test_draw_a_card()

--- test/blackjack_test.py
+++ test/blackjack_test.py
@@ -1,23 +1,36 @@
 from app.blackjack_game import draw_a_card
+import random
 
 
 def test_draw_a_card():
-    deck=[
-        {2,"clubs",2,2,1},
-        {3,"clubs",3,3,1},
-        {4,"clubs",4,4,1},
-        {5,"clubs",5,5,1},
-        {6,"clubs",6,6,1},
-        {7,"clubs",7,7,1},
-        {8,"clubs",8,8,1},
-        {9,"clubs",9,9,1},
-        {10,"clubs",10,10,1}
+    
+    deck = [
+    {"id" : 1, "suit": 'clubs', "card": 'Ace',"value": 11},
+    {"id" : 2, "suit": 'clubs', "card": '2', "value": 2 },
+    {"id" : 3, "suit": 'clubs', "card": '3', "value": 3 },
     ]
+    
+    assert type(deck) == list
 
+    # assert that the card is a dectionary and that it has certain keys we expect it to have
+    card = deck[0]
+    assert type(card) == dict
+    assert card["id"] == 1
+    assert card["suit"] == 'clubs'
+    assert card["card"] == 'Ace'
+    assert card["value"] == 11
 
-card , deck = draw_a_card(deck)
+    # count the number of remaining cards in active deck
+    remaining_cards = len(deck)
+    assert remaining_cards == 3
 
-#assert that the card is a dectionary and that it has certain keys we expect it to have
-#count the number of remaining cards in active deck.
-#assert that len is 1 less than deck was to start with to ensure we're removing a card from the deck
-#indented within the test function
+    # assert that len is 1 less than deck was to start with to ensure we're removing a card from the deck
+    card, deck = draw_a_card(deck)
+    remaining_cards = len(deck)
+    assert remaining_cards == 2
+
+    card, deck = draw_a_card(deck)
+    remaining_cards = len(deck)
+    assert remaining_cards == 1
+
+    # these assertions should be indented within the test function
